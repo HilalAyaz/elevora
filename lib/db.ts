5fb9beb5696e71bbe575c28224a6cd45ca36d7ca
Hilal Ayaz <134634880+HilalAyaz@users.noreply.github.com>
2025-09-24 01:42:04 +0500

    fixing deployement 2

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from "./generated/prisma/client.js";
+import { PrismaClient } from "./generated/prisma/client";
 
-export { Role } from "./generated/prisma/client.js";
+export { Role } from "./generated/prisma/client";
 
 declare global {
   var prisma: PrismaClient | undefined;
